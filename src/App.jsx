fd2a12704227b810dfae64a207fa72ebbdb3a0c5
Lucas Araujo <lucasaraujo4188@gmail.com>
2023-02-14 16:27:42 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Header } from './components/Header'
+import { Post } from './Post'
+
+import './styles.css'
+
+export function App () {
+  return (
+    <div>
+      <Header />
+      <Post
+        author='Lucas Araujo'
+        content='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet veniam sint laborum mollitia voluptas eum, voluptate rerum id dolor cupiditate deserunt, iusto possimus ipsam! Quisquam consequatur odio a tempore sunt.'
+      />
+      <Post author='Antonio Araujo' content='Um novo post muito legal' />
+    </div>
+  )
+}
