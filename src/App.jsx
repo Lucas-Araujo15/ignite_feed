cf054b4208083f33a335d5419a29c5c3da24d3b1
Lucas Araujo <lucasaraujo4188@gmail.com>
2023-02-15 19:29:15 -0300

    feat: estilizando Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import { Header } from './components/Header'
 import { Post } from './Post'
 
+import styles from './App.module.css'
+
 import './global.css'
+import { Sidebar } from './components/Sidebar'
 
 export function App () {
   return (
     <div>
       <Header />
-      <Post
-        author='Lucas Araujo'
-        content='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet veniam sint laborum mollitia voluptas eum, voluptate rerum id dolor cupiditate deserunt, iusto possimus ipsam! Quisquam consequatur odio a tempore sunt.'
-      />
-      <Post author='Antonio Araujo' content='Um novo post muito legal' />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author='Lucas Araujo'
+            content='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet veniam sint laborum mollitia voluptas eum, voluptate rerum id dolor cupiditate deserunt, iusto possimus ipsam! Quisquam consequatur odio a tempore sunt.'
+          />
+          <Post author='Antonio Araujo' content='Um novo post muito legal' />
+        </main>
+      </div>
     </div>
   )
 }
