05aa1f18c8a3abd70be2322c03b6f7324c6e7021
Lucas Araujo <lucasaraujo4188@gmail.com>
2023-02-16 00:12:04 -0300

    feat: estilizando post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -29,11 +29,13 @@ export function Post () {
         </p>
 
         <p>
-          👉 <a href='#'>jane.design/doctorcare</a>
+          👉{' '}<a href='#'>jane.design/doctorcare</a>
         </p>
 
         <p>
-          <a href='#'>#novoprojeto #nlw #rocketseat</a>{' '}
+          <a href='#'>#novoprojeto</a>{' '}
+          <a href='#'>#nlw</a>{' '}
+          <a href='#'>#rocketseat</a>
         </p>
       </div>
     </article>
