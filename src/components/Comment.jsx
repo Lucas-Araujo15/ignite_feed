048bbf67c1b85c7e6ddbbd602caf33e54bce5737
Lucas Araujo <lucasaraujo4188@gmail.com>
2023-02-16 15:42:17 -0300

    feat: adiciona componente Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,37 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment () {
+  return (
+    <div className={styles.comment}>
+      <img src='https://github.com/lucas-araujo15.png' alt='' />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Lucas Araujo</strong>
+              <time
+                title='15 de Fevereiro às 08:13h'
+                dateTime='2023-02-15 08:13:30'
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title='Deletar comentário'>
+              <Trash size={20} />
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
